31def7e7f109c223089610a284a5b627d9b62d4c
Jack <cba4125@outlook.com>
2020-09-21 13:46:57 -0700

    remove hide-all function

--- content.js
+++ content.js
@@ -4,37 +4,29 @@ let timer;
 chrome.runtime.onMessage.addListener((request) => {
   console.log('request received', request);
   if (request.action === 'hide') {
-    console.log('hide');
     player.style.visibility = 'hidden';
   }
 
   if (request.action === 'show') {
-    console.log('show');
     player.style.visibility = 'visible';
   }
 
   if (request.action === 'start') {
-    console.log('start');
     timer = setInterval(() => {
       document.querySelector('.ytp-next-button').click();
     }, 10000);
   }
 
   if (request.action === 'stop') {
-    console.log('stop');
     clearInterval(timer);
   }
 });
 
 // hide all
-chrome.storage.local.get('action', (response) => {
-  console.log('entered storage', response);
+// chrome.storage.local.get('action', (response) => {
+//   console.log('entered storage', response);
 
-  if (response.action === 'hide') {
-    player.style.visibility = 'hidden';
-  }
-
-  if (response.action === 'show') {
-    player.style.visibility = 'visible';
-  }
-});
+//   if (response.action === 'hide') {
+//     player.style.visibility = 'hidden';
+//   }
+// });
--- popup/popup.js
+++ popup/popup.js
@@ -1,54 +1,46 @@
-let autoplay = document.querySelector('.autoplay-state');
+const autoplay = document.querySelector('.autoplay-state');
+const alertDisplay = document.querySelector('.alert');
 
-document.getElementById('hide').addEventListener('click', () => {
-  console.log('clicked');
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    // first parameter of sendMessage targets only the tab
-    // where the extension was clicked on
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'hide' });
-    chrome.storage.local.set({ action: 'hide' });
-  });
-});
+// hide screen
+document
+  .getElementById('hide')
+  .addEventListener('click', () => screenControl('hide'));
+document
+  .getElementById('show')
+  .addEventListener('click', () => screenControl('show'));
 
-document.getElementById('show').addEventListener('click', () => {
-  console.log('clicked');
+function screenControl(control) {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'show' });
-    chrome.storage.local.set({ action: 'show' });
+    if (/^https:\/\/www.youtube.com\/watch/.test(tabs[0].url)) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: control });
+      chrome.storage.local.set({ action: control });
+    } else {
+      alertDisplay.innerHTML = 'Sorry you can only do this on a video page';
+    }
   });
-});
+}
+
+// autoplay
+document
+  .getElementById('start')
+  .addEventListener('click', () => autoplayControl('start'));
+document
+  .getElementById('stop')
+  .addEventListener('click', () => autoplayControl('stop'));
+
+function autoplayControl(control) {
+  autoplay.innerHTML = 'Autoplay is running...';
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: control });
+  });
+}
 
 // hide all
 // doesn't work on new tabs, because they have new ids, aren't included
-document.getElementById('hide-all').addEventListener('click', () => {
-  console.log('clicked');
-  chrome.tabs.query({ url: 'https://*.youtube.com/*' }, (tabs) => {
-    tabs.forEach((tab) => chrome.tabs.sendMessage(tab.id, { action: 'hide' }));
-    chrome.storage.local.set({ action: 'hide' });
-  });
-});
-
-document.getElementById('show-all').addEventListener('click', () => {
-  console.log('clicked');
-  chrome.tabs.query({ url: 'https://*.youtube.com/*' }, (tabs) => {
-    tabs.forEach((tab) => chrome.tabs.sendMessage(tab.id, { action: 'show' }));
-    chrome.storage.local.set({ action: 'show' });
-  });
-});
-
-// auto next
-document.getElementById('start').addEventListener('click', () => {
-  console.log('clicked');
-  autoplay.innerHTML = 'Autoplay is running...';
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'start' });
-  });
-});
-
-document.getElementById('stop').addEventListener('click', () => {
-  console.log('clicked');
-  autoplay.innerHTML = '';
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: 'stop' });
-  });
-});
+// document.getElementById('hide-all').addEventListener('click', () => {
+//   console.log('clicked');
+//   chrome.tabs.query({ url: 'https://*.youtube.com/*' }, (tabs) => {
+//     tabs.forEach((tab) => chrome.tabs.sendMessage(tab.id, { action: 'hide' }));
+//     chrome.storage.local.set({ action: 'hide' });
+//   });
+// });
